81fe6c4b4f0f31945f5bf7ec65331f6842202ac0
Jakob Björns <jakob@lappis.bjrns.nu>
2024-11-30 12:31:15 +0100

    from_date to click

--- bean_importers/avanza_downloader.py
+++ bean_importers/avanza_downloader.py
@@ -22,14 +22,20 @@ class Avanza_jabs(Avanza):
               type=click.Path(file_okay=False, dir_okay=True, exists=True),
               default='.',
               help='Output directory.')
-def main(output_dir):
+@click.option('--from_date', '-f',
+              type=click.DateTime(),
+              default=None,
+              help='Fetch transactions from this date.')
+def main(output_dir, from_date):
+    if from_date:
+        from_date = from_date.date()
     load_dotenv()
     avanza = Avanza_jabs({
         'username': os.getenv("avanza_username"),
         'password': os.getenv("avanza_password"),
         'totpSecret': os.getenv("avanza_totpSecret")
     })
-    download_transactions(avanza, output_dir)
+    download_transactions(avanza, output_dir, from_date)
     download_positions(avanza, output_dir)
 
 
@@ -43,9 +49,8 @@ def download_positions(avanza: Avanza_jabs, output_dir):
     with open(pos_file, "wb") as testfil:
         testfil.write(s)
 
-
 def download_transactions(avanza: Avanza_jabs, output_dir, transactions_from=None):
-    b = avanza.get_transactions_details(transactions_from=transactions_from)
+    b = avanza.get_transactions_details(transactions_from=transactions_from,max_elements=100000)
     trx_file = os.path.join(output_dir, "ava_transactions.json")
     with open(trx_file, "w") as testfil:
         json.dump(b, testfil)
