2dc3e5f83f134813e1ace068f8d4f372a2b66972
Jakob Björns <jakob_b99@live.se>
2026-02-28 15:24:57 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="bean_importers",
-      version=0.10,
+      version=0.11,
       packages=find_packages(),
       install_requires=[
           "avanza-api>=15.0.0",
